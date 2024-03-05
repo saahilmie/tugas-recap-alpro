efa2f88ea47cdf7675390d9c280ff97c2f2b2ee4
saahilmie <161787534+saahilmie@users.noreply.github.com>
2024-03-05 19:06:11 +0700

    Update percobaan2b.c

---
+++ Unit 1/percobaan2b.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+  printf(Alpro itu mudah!);
+  return 0;
+}
+
+
+/*Terdapat kesalahan di baris 4, seharusnya printf("Alpro itu mudah!"); karena dia mau dibuat menjadi kalimat*/
